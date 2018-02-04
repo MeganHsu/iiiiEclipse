f3dc88e41ecbd314d07ef91cd9943ba9c54bf015
Meganhsu <dogdog2250@gmail.com>
2018-02-04 15:13:44 +0800

    first test

---
+++ iiiiGitHub/src/iiiiGitHub/firstTest.java
@@ -0,0 +1,10 @@
+package iiiiGitHub;
+
+public class firstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a test file.");
+	}
+
+}
