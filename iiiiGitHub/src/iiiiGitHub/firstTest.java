6cac0cdffe1164c1c495a0d4a30365eef9c409a1
MeganHsu <dogdog2250@gmail.com>
2018-02-04 15:32:04 +0800

    second test

--- iiiiGitHub/src/iiiiGitHub/firstTest.java
+++ iiiiGitHub/src/iiiiGitHub/firstTest.java
@@ -4,7 +4,8 @@ public class firstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is a test file.");
+System.out.println("This is a test file."
+		+ " Second test.");
 	}
 
 }
